ea62ed4581a7b09ab653e99db04b811c938e7b18
suryatejaj97 <suryatejaj97@gmail.com>
2022-12-14 04:07:50 +0530

    first commit

---
+++ 01-globals.js
@@ -0,0 +1,5 @@
+// Globals - These objects are global in nature and available in all modules
+// These can used directly in your application without using any imports
+
+// List of globals objects - Console, __dirname, __filename, setTimeout() etc...
+
