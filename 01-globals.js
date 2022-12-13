3d4f25af06c2c6113f706215cc8639747993cfed
suryatejaj97 <suryatejaj97@gmail.com>
2022-12-14 04:43:09 +0530

    globals

--- 01-globals.js
+++ 01-globals.js
@@ -1,5 +1,12 @@
 // Globals - These objects are global in nature and available in all modules
 // These can used directly in your application without using any imports
 
-// List of globals objects - Console, __dirname, __filename, setTimeout() etc...
+// No Window object in Node.js
 
+// List of globals objects - Console, __dirname, __filename, setInterval(), require, process, etc...
+
+console.log('Current directory path --- ' + __dirname)
+
+setInterval(()=> {
+    console.log('Hey there, Welcome to Globals Sessions')
+}, 3000)
