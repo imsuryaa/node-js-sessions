58e7b47cb1fddf3b008fb9985a174a156c2bf485
suryatejaj97 <suryatejaj97@gmail.com>
2022-12-25 13:04:41 +0530

    http module

---
+++ 07-httpModule.js
@@ -0,0 +1,3 @@
+// HTTP Module - It is a built-in module in Node.js to transfer data and handle requests and responses.
+// Two types - HTTP & HTTPS
+// createServer() method is used to create the HTTP server
